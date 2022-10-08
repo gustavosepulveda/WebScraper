6f79593b556f07ecf98df528331ae63ed58597e1
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2022-10-08 00:09:09 -0500

    frontend started

--- index.js
+++ index.js
@@ -7,28 +7,35 @@ const app = express();
 
 const url = "https://www.theguardian.com/us";
 
-app.METHOD(PATH, HANDLER)
+//app.METHOD(PATH, HANDLER)
 
-app.get("/", function() {
-	
+app.get("/", function(req, res) {
+	res.json('This is my webscrapper')
 })
 
-
-axios(url)
+app.get("/results", (req, res) => {
+	axios(url)
 	.then((response) => {
 		const html = response.data;
 		const srch = cheerio.load(html);
 		const articles = [];
+
 		srch(".fc-item__title", html).each(function () {
 			const title = srch(this).text();
 			const url = srch(this).find("a").attr("href");
 			articles.push({
 				title,
 				url,
-			});
-			console.log(articles);
-		});
-	})
-	.catch((err) => console.log(err));
+			})
+		})
+		res.json(articles)
+	})	.catch((err) => console.log(err));
+	
+	
+});
+
+
+
+
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT} `));
